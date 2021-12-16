d92e5bbf35f0e41a4a024ed9843a4623179c6f9b
Erik Westrup <erik.westrup@gmail.com>
2021-12-16 19:44:17 +0100

    Working !rot13command

--- tardsquad_discord_bot/__main__.py
+++ tardsquad_discord_bot/__main__.py
@@ -1,23 +1,43 @@
 # Entry point when called as a module
 # $ python -m taiga_stats
 
+import codecs
 import os
 import sys
 
 import discord
 from dotenv import load_dotenv
 
+# @discord.client.event
+# async def on_ready():
+# print(f"{client.user} has connected to Discord!")
 
-@client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+
+class MyClient(discord.Client):
+    def __init__(self, guild, *args, **kwargs):
+        self.guild = guild
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        print(f"Logged on as {self.user} at {self.guild}")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith("!rot13"):
+            text = message.content[len("!rot13") :]
+            textrot = codecs.encode(text, "rot_13")
+            reply = "*rot13 of that is:*\n{:s}".format(textrot)
+            await message.channel.send(reply)
 
 
 def main():
     load_dotenv()
     TOKEN = os.getenv("DISCORD_TOKEN")
+    GUILD = os.getenv("GUILD")
 
-    client = discord.Client()
+    client = MyClient(GUILD)
     client.run(TOKEN)
 
 
