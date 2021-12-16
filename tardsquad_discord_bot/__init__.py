9d9f5624fc2a0ff854ef3a76f1f1ee20cdede127
Erik Westrup <erik.westrup@gmail.com>
2021-12-16 23:41:46 +0100

    WIP roboust version definition

--- tardsquad_discord_bot/__init__.py
+++ tardsquad_discord_bot/__init__.py
@@ -1,8 +1,14 @@
-try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8. Reference: https://stackoverflow.com/a/59734959/265508
-    import importlib_metadata as metadata
+# try:
+# from importlib import metadata
+# except ImportError:  # for Python<3.8. Reference: https://stackoverflow.com/a/59734959/265508
+# import importlib_metadata as metadata
 
-# Read version from pyproject.toml. Might not be correct in dev enviorment, but will be when distributed.
-# Reference: https://stackoverflow.com/a/67097076/265508
-__version__ = metadata.version("tardsquad-discord-bot")
+# # Read version from pyproject.toml. Might not be correct in dev enviorment, but will be when distributed.
+# # Reference: https://stackoverflow.com/a/67097076/265508
+# __version__ = metadata.version("tardsquad-discord-bot")
+
+from pathlib import Path
+
+from single_source import get_version
+
+__version__ = get_version(__name__, Path(__file__).parent.parent)
