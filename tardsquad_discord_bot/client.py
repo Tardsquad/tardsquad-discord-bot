d21998cfde0fcc2517a18ddbccc85b014e2634bc
Erik Westrup <erik.westrup@gmail.com>
2021-12-17 21:57:35 +0100

    Add Ljunghusen intercepter

--- tardsquad_discord_bot/client.py
+++ tardsquad_discord_bot/client.py
@@ -1,5 +1,6 @@
 import codecs
 import logging
+import re
 from operator import itemgetter
 
 import discord
@@ -12,13 +13,7 @@ class TardsquadClient(discord.Client):
         self.guild = guild
         super().__init__(*args, **kwargs)
 
-    async def on_ready(self):
-        logging.info(f"Logged on as {self.user} at server {self.guild}.")
-
-    async def on_message(self, message):
-        if message.author == self.user or message.content[0] != "!":
-            return
-
+    async def _handle_commands(self, message):
         if message.content.startswith("!help"):
             commands = [
                 ("rot13", "Encode the string after the command with the rot-13 scheme."),
@@ -28,8 +23,7 @@ class TardsquadClient(discord.Client):
             commandstr = "\n".join(f"**!{cmd}** - {desc}" for cmd, desc in sorted(commands, key=itemgetter(0)))
             reply = "The commands that I support are:\n{:s}".format(commandstr)
             await message.channel.send(reply)
-
-        if message.content.startswith("!rot13 "):
+        elif message.content.startswith("!rot13 "):
             text = message.content[len("!rot13 ") :]
             textrot = codecs.encode(text, "rot_13")
             reply = "*rot13 of that is:*\n{:s}".format(textrot)
@@ -37,3 +31,19 @@ class TardsquadClient(discord.Client):
         elif message.content.startswith("!version"):
             reply = f"I'm at version `{tardsquad_discord_bot.__version__}`"
             await message.channel.send(reply)
+
+    async def _intercept(self, message):
+        if re.search(r"\bljunghusen|höllviken\b", message.content, re.I):
+            await message.channel.send("Are you sure it was on the right side of the Kanalen?")
+
+    async def on_ready(self):
+        logging.info(f"Logged on as {self.user} at server {self.guild}.")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content[0] == "!":
+            await self._handle_commands(message)
+        else:
+            await self._intercept(message)
