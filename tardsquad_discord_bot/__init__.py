087caab641d90b73754af2e333f705914659c4ec
Erik Westrup <erik.westrup@gmail.com>
2021-12-18 22:47:33 +0100

    TRY without single_source

--- tardsquad_discord_bot/__init__.py
+++ tardsquad_discord_bot/__init__.py
@@ -1,5 +1,3 @@
-from pathlib import Path
+# from single_source import get_version
 
-from single_source import get_version
-
-__version__ = get_version(__name__, Path(__file__).parent.parent)
+__version__ = "4.0.0"
