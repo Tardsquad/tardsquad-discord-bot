3508ecb8aa3f4b57c8bfaebc62ec17caaf40652a
Erik Westrup <erik.westrup@gmail.com>
2022-05-02 11:33:27 +0200

    Added: bin/gcp_contaner_cleanup.sh

---
+++ bin/gcp_contaner_cleanup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Implementation of "retention policy" of built contaienrs at
+# https://console.cloud.google.com/gcr/images/tardsquad-discord-bot/global/tardsquad-discord-bot?project=tardsquad-discord-bot
+# Ref: https://serverfault.com/a/952644
+
+IMAGE_NAME=gcr.io/tardsquad-discord-bot/tardsquad-discord-bot
+RETENTION_IMAGES=5  # Keep X latest images.
+
+digests=$(gcloud container images list-tags $IMAGE_NAME --format=json | awk '/digest/{ print $2 }' | sed -e 's/^"//' -e 's/.\{2\}$//' | tail -n +$((RETENTION_IMAGES+1)))
+test -n "$digest" || exit 0
+
+for digest in "$digests"; do
+		#gcloud container images -q delete $IMAGE_NAME@$digest;
+		echo "$digest"
+		test $? -eq 0 && echo "Deleted $digest" || echo "ERROR deleting $diest" >&2
+done;
